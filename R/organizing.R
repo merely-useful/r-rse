9eeb0a15cc300d2a0fa719676c62d5862aa17ca7
Luke W. Johnston <lwjohnst@gmail.com>
2020-12-03 12:15:10 +0100

    helper r files

---
+++ R/organizing.R
@@ -0,0 +1,10 @@
+library(tidyverse)
+
+"
+
+## Final exercise
+
+<!-- NOTE: Preferably something linked to working towards completing the project assignments, maybe to be used as a milestone? -->
+
+" %>%
+    walk2(chapters, write_lines, append = TRUE)
